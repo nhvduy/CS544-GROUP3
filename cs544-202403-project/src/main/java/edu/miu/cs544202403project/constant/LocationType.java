8e218ddd6a7efbeab3c37928aeeb7520d69ecc84
hnin-hnin-wai <50986766+hnin-hnin-wai@users.noreply.github.com>
2024-03-16 18:43:40 -0500

    remove extra tables

---
+++ cs544-202403-project/src/main/java/edu/miu/cs544202403project/constant/LocationType.java
@@ -0,0 +1,6 @@
+package edu.miu.cs544202403project.constant;
+
+public enum LocationType {
+    DINNING, CLASSROOM, LIBRARY, GYM
+
+}
---
+++ cs544-202403-project/src/test/java/edu/miu/cs544202403project/Cs544202403ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544202403project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs544202403ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
