88af380c27cd6f9c227d406394a950d1a2ca03de
hnin-hnin-wai <50986766+hnin-hnin-wai@users.noreply.github.com>
2024-03-17 14:17:24 -0500

    Delete cs544-202403-project directory

--- cs544-202403-project/cs544-202403-project/src/main/java/edu/miu/cs544202403project/constant/LocationType.java
+++
@@ -1,6 +0,0 @@
-package edu.miu.cs544202403project.constant;
-
-public enum LocationType {
-    DINNING, CLASSROOM, LIBRARY, GYM
-
-}
--- cs544-202403-project/src/main/java/edu/miu/cs544202403project/constant/AccountType.java
+++
@@ -1,5 +0,0 @@
-package edu.miu.cs544202403project.constant;
-
-public enum AccountType {
-    DINNING, ATTENDANCE, VIRTUAL_DOLLAR, REC_CENTER
-}
