2f3911bab4699bf31341dcf9979d466d7c38f680
xuzhi <compro.xwei@gmail.com>
2024-03-19 11:42:21 -0500

    add some code

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/SessionController.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/SessionController.java
@@ -1,10 +1,13 @@
 package edu.miu.cs.cs544.controller;
 
 import edu.miu.common.controller.BaseReadWriteController;
-import edu.miu.cs.cs544.domain.Member;
+import edu.miu.cs.cs544.domain.*;
+import edu.miu.cs.cs544.service.EventService;
 import edu.miu.cs.cs544.service.SessionService;
 import edu.miu.cs.cs544.service.contract.MemberPayload;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +18,19 @@ public class SessionController extends BaseReadWriteController<MemberPayload, Me
         @Autowired
        private SessionService sessionService;
 
+        @Autowired
+        private EventService eventService;
+
         //add session
 
 
+        @GetMapping("/{sessionId}")
+        public Session getSession(@PathVariable("eventId") Integer eventId,@PathVariable("sessionId") Integer sessionId){
+           List<Session> sessionList =  eventService.getEventById(eventId).getSchedule();
+           for(Session session : sessionList){
+               if(session.getSessionId() == sessionId){
+                   return ResponseEntity(session,HttpStatus.OK);
+               }
+           }
+        }
 }
