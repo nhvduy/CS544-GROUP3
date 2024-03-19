29cb3c5280403a52d7f653699cfbcf2756d6a26d
hnin-hnin-wai <50986766+hnin-hnin-wai@users.noreply.github.com>
2024-03-19 18:25:50 -0500

    add resources

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/ScanRecordServiceImpl.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/ScanRecordServiceImpl.java
@@ -3,10 +3,23 @@ package edu.miu.cs.cs544.service;
 import edu.miu.common.service.BaseReadWriteServiceImpl;
 import edu.miu.cs.cs544.domain.ScanRecord;
 import edu.miu.cs.cs544.domain.Scanner;
+import edu.miu.cs.cs544.repository.ScanRecordRepository;
 import edu.miu.cs.cs544.service.contract.ScanRecordPayload;
 import edu.miu.cs.cs544.service.contract.ScannerPayload;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ScanRecordServiceImpl extends BaseReadWriteServiceImpl<ScanRecordPayload, ScanRecord,Integer> implements ScanRecordService{
+
+    @Autowired
+    private ScanRecordRepository scanRecordRepository;
+
+
+    @Override
+    public List<ScanRecordPayload> getScanRecordByScannerCode(Integer scannerCode) {
+        return null;
+    }
 }
