0cb505dcc7fad0821214d18df24d9d140c80dfc9
Muneeb Ahmed <muneeb.ahmed09@gmail.com>
2024-03-19 10:59:11 -0500

    add resources

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/contract/AccountPayload.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/contract/AccountPayload.java
@@ -1,2 +1,13 @@
-package edu.miu.cs.cs544.service.contract;public class AccountPayload {
+package edu.miu.cs.cs544.service.contract;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class AccountPayload implements Serializable {
+    private Integer accountId;
+    private  String name;
+    private  String description;
+    private Integer balance;
 }
