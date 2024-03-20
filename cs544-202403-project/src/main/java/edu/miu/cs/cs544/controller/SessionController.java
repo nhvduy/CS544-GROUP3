9819d59ae3ce1c3e0add996435587c3194062520
tsigereda-hagos <150645322+tsigereda-hagos@users.noreply.github.com>
2024-03-19 21:29:07 -0500

    added

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/SessionController.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/SessionController.java
@@ -2,7 +2,6 @@ package edu.miu.cs.cs544.controller;
 
 import edu.miu.common.controller.BaseReadWriteController;
 import edu.miu.cs.cs544.domain.Member;
-import edu.miu.cs.cs544.service.EventService;
 import edu.miu.cs.cs544.service.SessionService;
 import edu.miu.cs.cs544.service.contract.MemberPayload;
 import edu.miu.cs.cs544.service.contract.SessionPayload;
--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/EventService.java
+++
@@ -1,9 +0,0 @@
-package edu.miu.cs.cs544.service;
-
-import edu.miu.common.service.BaseReadWriteService;
-import edu.miu.cs.cs544.domain.Event;
-import edu.miu.cs.cs544.service.contract.EventPayload;
-
-public interface EventService extends BaseReadWriteService<EventPayload, Event, Long> {
-    Event getByEventId(Integer eventId);
-}
