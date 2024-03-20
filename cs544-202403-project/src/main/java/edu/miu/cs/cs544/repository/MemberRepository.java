4b4737aafbcba07128adfa055e000e1baf0a2ce4
NahomBirhane <nahomyatus@gmail.com>
2024-03-20 11:10:07 -0500

    some changes from scanRecord to Attendance

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/repository/MemberRepository.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/repository/MemberRepository.java
@@ -2,7 +2,9 @@ package edu.miu.cs.cs544.repository;
 
 import edu.miu.common.repository.BaseRepository;
 import edu.miu.cs.cs544.domain.Member;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends BaseRepository<Member, Integer>{
 
 }
--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/contract/MemberPayload.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/contract/MemberPayload.java
@@ -12,11 +12,9 @@ public class MemberPayload implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer memberId;
-	private  String firstName;
+	private String firstName;
 	private String lastName;
-
 	private String email;
-
 	private String barCode;
 
 }
