a5e702eee05f2faac38de2e04f632fbe45b8767d
tsigereda-hagos <150645322+tsigereda-hagos@users.noreply.github.com>
2024-03-20 14:19:31 -0500

    added test

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/EventService.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/EventService.java
@@ -1,10 +1,16 @@
 package edu.miu.cs.cs544.service;
 
 import edu.miu.common.service.BaseReadWriteService;
+import edu.miu.cs.cs544.domain.Attendance;
 import edu.miu.cs.cs544.domain.Event;
+import edu.miu.cs.cs544.domain.Member;
 import edu.miu.cs.cs544.service.contract.EventPayload;
 
+import java.util.List;
+import java.util.Map;
+
 public interface EventService extends BaseReadWriteService<EventPayload, Event, Integer> {
+    Map<Member, List<Attendance>> calculateAttendance(Integer eventId);
     Event getByEventId(Integer eventId);
 
 }
